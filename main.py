33c7d264e01f501e3477c915e9a49f93d6cad131
leo_proger <87581982+leo-proger@users.noreply.github.com>
2026-01-28 11:52:49 +0300

    feat: add ffmeg requirement check

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import platform
+import shutil
+
 from downloader import *
 
 
 def main():
+    if not check_ffmpg():
+        os_name = platform.system()
+        base_msg = "Please install ffmpeg. Guide - https://github.com/oop7/ffmpeg-install-guide?tab=readme-ov-file"
+        match os_name:
+            case "Windows":
+                print(base_msg + "#windows-installation")
+
+            case "Darwin":
+                print(base_msg + "#macos-installation")
+
+            case "Linux":
+                print(base_msg + "#linux-installation")
+
+        return
+
     user_input = int(input("""Choose number:
 1. Download MP4
 2. Download MP3
@@ -32,9 +50,6 @@ def main():
         )
 
     elif user_input == 2:
-        if not check_ffmpg(): # TODO
-            ...
-
         print("Your video has been started downloading...")
 
         download(
@@ -48,7 +63,8 @@ def main():
 
 
 def check_ffmpg() -> bool:
-    ...
+    return shutil.which("ffmpeg") is not None
+
 
 if __name__ == "__main__":
     main()
